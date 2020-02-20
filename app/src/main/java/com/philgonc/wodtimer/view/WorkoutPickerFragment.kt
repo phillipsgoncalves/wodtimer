9ce98f69c8806cc34a610f9ac052aec3a36b0836
phillipsgoncalves <ktrwnfgoncalves@gmail.com>
2020-02-20 22:15:50 +0000

    Created workout picker fragment

---
+++ app/src/main/java/com/philgonc/wodtimer/view/WorkoutPickerFragment.kt
@@ -0,0 +1,24 @@
+package com.philgonc.wodtimer.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class WorkoutPickerFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
